96fe4cb29ba201363a52f404f847ecc8903d0a75
StephaneBek <selloiltoday@live.ca>
2025-01-26 01:41:03 -0500

    Create Skill.cs
    create a class for skills

---
+++ Assets/Skills/Skill.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Skill
+{
+    public string skillName;
+    public int level;
+    public int maxLevel;
+    public float effectModifier;
+}
